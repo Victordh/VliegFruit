37755a29fe06abefdcc6743498c1bbe2a584cd92
Victor <vdenhaan@gmail.com>
2015-11-04 18:20:31 +0100

    added writing to output file, slight change to while loop to allow for writing correctly

--- Project/insertion_sort.py
+++ Project/insertion_sort.py
@@ -3,22 +3,30 @@ x = list(x)
 y = 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
 y = list(y)
 
+# open overwrites the already existing file
+f = open('out.txt', 'w')
+
 def sort_numbers(s):
     count = 0
     for i in range(1, len(s)):
         val = s[i]
         j = i - 1
         while (j >= 0) and (s[j] > val):
+            f.write(str(s) + "\n")
             count += 1
+            temp = s[j+1]
             s[j+1] = s[j]
+            s[j] = temp
             j = j - 1
-        s[j+1] = val
-    print count 
+    f.write("Amount of steps needed: " + str(count))
     return s
 
 
-print sort_numbers(y)
- 
+sort_numbers(x)
+f.write("\n\n")
+sort_numbers(y)
+
+f.close()
 
 # def main():
 #     x = eval(input("Enter numbers to be sorted: "))
