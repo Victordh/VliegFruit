dee18e7ccbee46120881def253c690c7c6e74f6a
Victor <vdenhaan@gmail.com>
2015-11-06 13:36:17 +0100

    merged swap.py with functions

--- Project/functions.py
+++ Project/functions.py
@@ -43,3 +43,20 @@ def big_flip(s):
     
     # closes the output file
     f.close()
+
+# swaps in list [i] the position of the [size] amount of numbers starting on position [pos]
+def swap(size, pos, i):
+	
+	y = []
+	count = 0
+	while count < size:
+		y.append(i[count + pos])
+		count += 1
+	#print y
+	y.reverse()
+	print y
+	count = 0
+	while count < size:
+		i[count + pos] = y[count]
+		count += 1
+	return i
