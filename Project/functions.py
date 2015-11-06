292a55b77ae7f3ccf73fb326b85b0ba786f3f40a
Victor <vdenhaan@gmail.com>
2015-11-06 14:08:58 +0100

    added global variable counter, removed big_flip because it was obsolete

--- Project/functions.py
+++ Project/functions.py
@@ -1,62 +1,52 @@
+import global_variables
+
 def insertion_sort(s):
     # append ('a') doesn't overwrite the already existing file
     f = open('out.txt', 'a')
     
-    count = 0
+    #counter = 0
     for i in range(1, len(s)):
         val = s[i]
         j = i - 1
         while (j >= 0) and (s[j] > val):
             f.write(str(s) + "\n")
-            count += 1
+            global_variables.counter += 1
             temp = s[j+1]
             s[j+1] = s[j]
             s[j] = temp
             j = j - 1
     # prints final (correct) list
     f.write(str(s) + "\n")
-    f.write("Amount of steps needed: " + str(count))
+    f.write("Amount of steps needed: " + str(global_variables.counter))
     f.write("\n\n")
     return s
     
     # closes the output file
     f.close()
 
-def big_flip(s):
+# swaps in list [s] the position of the [size] amount of numbers starting on position [pos]
+def swap(size, pos, s):
     # append ('a') doesn't overwrite the already existing file
     f = open('out.txt', 'a')
     
-    # TODO make count global and don't reset it when combining different sorts
-    count = 1
-    length = len(s)
-    # prints first (incorrect) list
     f.write(str(s) + "\n")
-    for i in range(0, length / 2):
-        temp = s[length - i - 1]
-        s[length - i - 1] = s[i]
-        s[i] = temp
-    # prints final (correct) list
+    global_variables.counter += 1
+    y = []
+    count = 0
+    while count < size:
+        y.append(s[count + pos])
+        count += 1
+    #print y
+    y.reverse()
+    #print y
+    count = 0
+    while count < size:
+        s[count + pos] = y[count]
+        count += 1
     f.write(str(s) + "\n")
-    f.write("Amount of steps needed: " + str(count))
+    f.write("Amount of steps needed: " + str(global_variables.counter))
     f.write("\n\n")
     return s
     
     # closes the output file
     f.close()
-
-# swaps in list [i] the position of the [size] amount of numbers starting on position [pos]
-def swap(size, pos, i):
-	
-	y = []
-	count = 0
-	while count < size:
-		y.append(i[count + pos])
-		count += 1
-	#print y
-	y.reverse()
-	print y
-	count = 0
-	while count < size:
-		i[count + pos] = y[count]
-		count += 1
-	return i
--- Project/main.py
+++ Project/main.py
@@ -1,5 +1,8 @@
+import global_variables
 from functions import *
 import random
+
+global_variables.initialise()
 
 # overwrites out.txt and closes right away for an empty file
 f = open('out.txt', 'w')
@@ -9,14 +12,12 @@ x = random.sample(xrange(1, 26), 25)
 y = random.sample(xrange(1, 26), 25)
 z = random.sample(xrange(1, 26), 25)
 
-# x = 23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20, 5, 8, 18, 12, 13, 14, 15 ,16, 17, 21, 3, 4, 9
-# x = list(x)
-# y = 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
-# y = list(y)
-# z = list(y)
-
+global_variables.counter = 0
 insertion_sort(x)
+
+global_variables.counter = 0
 insertion_sort(y)
 
-big_flip(z)
+global_variables.counter = 0
+swap(25, 0, z)
 insertion_sort(z)
