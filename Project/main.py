3bb3ab6fafbbe912d66a4d2676c3a7d8091230fd
Victor <vdenhaan@gmail.com>
2015-11-10 17:12:45 +0100

    uses random lists now

--- Project/main.py
+++ Project/main.py
@@ -8,16 +8,8 @@ global_variables.initialise()
 f = open('out.txt', 'w')
 f.close()
 
-x = random.sample(xrange(1, 26), 25)
-y = random.sample(xrange(1, 26), 25)
-z = random.sample(xrange(1, 26), 25)
-
-global_variables.counter = 0
-insertion_sort(x)
-
-global_variables.counter = 0
-insertion_sort(y)
-
-global_variables.counter = 0
-swap(25, 0, z)
-insertion_sort(z)
+# 25 random lists
+for i in range(25):
+    n_random_list = random.sample(xrange(1, 26), 25)
+    global_variables.counter = 0
+    selection_sort(n_random_list)
