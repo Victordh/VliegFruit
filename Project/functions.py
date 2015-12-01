13c5ad70546c58d2cba509a8d74db07aa02f224a
Victor <vdenhaan@gmail.com>
2015-12-01 16:38:23 +0100

    added function that merges elements that should be next to eachother

--- Project/functions.py
+++ Project/functions.py
@@ -50,6 +50,42 @@ def sum_distance_neighbours(genome):
         sum += temp
     return sum
 
+genome = [[1], [2], [3], [5], [6], [4], [7], [9], [8], [10]]
+genome2 = [[5], [4], [3], [1], [2], [8], [10], [9], [6], [7]]
+
+# takes a genome and returns it with neighbours that should be next to
+# eachother merged
+def merge_correct_neighbours(genome, i):
+    better = list(genome)
+    if i < (len(genome) - 1):
+        # merges if element on the right is 1 higher than current element
+        if (genome[i][len(genome[i]) - 1] == genome[i + 1][0] - 1):
+            genome[i] = genome[i] + genome[i + 1]
+            genome.pop(i + 1)
+            merge_correct_neighbours(genome, i)
+        # merges if element on the right is 1 lower than current element
+        elif(genome[i][len(genome[i]) - 1] == genome[i + 1][0] + 1):
+            genome[i] = genome[i] + genome[i + 1]
+            genome.pop(i + 1)
+            merge_correct_neighbours(genome, i)
+        # moves to the next element
+        else:
+            merge_correct_neighbours(genome, i + 1)
+    return genome
+
+# better = list(genome)
+# print better
+# print genome[0][len(genome[0]) - 1]
+# print genome[0 + 1][0]
+# print genome[1][len(genome[1]) - 1]
+# print genome[1 + 1][0]
+# better[0] = genome[0] + genome[1]
+# print better
+# better.pop(1)
+# print better
+print merge_correct_neighbours(genome, 0)
+print merge_correct_neighbours(genome2, 0)
+
 #def shorten_genome(genome):
     # if numbers are already on the correct spot (only at start or end),
     # make the genome shorter and use the shorter one instead
