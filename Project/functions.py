3ab6d158b3395c45d7f0524255ec642e2d1a2ec8
Iris de Vries <Iris@wcw-stud-145-109-26-74.wireless.uva.nl>
2015-12-02 13:27:35 +0100

    niks bijzonders veranderd

--- Project/functions.py
+++ Project/functions.py
@@ -50,6 +50,26 @@ def sum_distance_neighbours(genome):
         Sum += temp
     return Sum
 
+# takes a genome and returns it with neighbours that should be next to
+# eachother merged (default value of i (0) shouldn't have to be changed)
+def merge_correct_neighbours(genome, i = 0):
+    better = list(genome)
+    if i < (len(genome) - 1):
+        # merges if element on the right is 1 higher than current element
+        if (genome[i][len(genome[i]) - 1] == genome[i + 1][0] - 1):
+            genome[i] = genome[i] + genome[i + 1]
+            genome.pop(i + 1)
+            merge_correct_neighbours(genome, i)
+        # merges if element on the right is 1 lower than current element
+        elif(genome[i][len(genome[i]) - 1] == genome[i + 1][0] + 1):
+            genome[i] = genome[i] + genome[i + 1]
+            genome.pop(i + 1)
+            merge_correct_neighbours(genome, i)
+        # moves to the next element
+        else:
+            merge_correct_neighbours(genome, i + 1)
+    return genome
+
 #def shorten_genome(genome):
     # if numbers are already on the correct spot (only at start or end),
     # make the genome shorter and use the shorter one instead
